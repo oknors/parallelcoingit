49667e502667ae59136215cc3da0d37e72fff4f1
marcetin <marcetin@gmail.com>
2021-01-01 18:46:13 +0100

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../node_modules/bulma/css/bulma.min.css";
+  import "../node_modules/bulma-helpers/css/bulma-helpers.min.css";
+  import { Router, Link, Route } from "svelte-routing";
+  // import Home from "./pages/1Home.svelte1";
+  import Home from "./pages/Home.svelte";
+
+  export let url = "";
+</script>
+
+<Home></Home>
+
